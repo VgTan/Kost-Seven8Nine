ccef08203a3f468c29afce3ed7aedabd65f4244a
VgTan <alfonsusvega9@gmail.com>
2023-11-28 17:05:52 +0700

    feat:

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,55 +1,40 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../css/header.css">
-</head>
-
-<body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="d-flex align-items-center">
-            <a class="navbar-brand" href="/"><img src="/images/logo.png" alt=""></a>
+<link rel="stylesheet" href="/css/header.css">
+<div class="header-container">
+    <div class="header">
+        <div class="name">
+            <a href="/" class="logo"><img src="/images/logo.png" alt=""></a>
         </div>
-        <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="/">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Find Room</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/aboutus">About Us</a>
-                </li>
-                <li class="nav-item" style="margin-right: 35px;">
-                    <a class="nav-link" href="#">Contact Us</a>
-                </li>
-                <li>
-                    <div class="header-button">
-                        @guest
-                        <a class="sign-button" href="/login">
-                            <p class="">Login</p>
-                            <img src="/images/contact.png" alt="">
-                        </a>
-                        @endguest
-                        @auth
-                        <a class="sign-button" href="/profile">
-                            <p class="">Profile</p>
-                            <img src="/images/contact.png" alt="">
-                        </a>
-                        @endauth
-                    </div>
-                </li>
-            </ul>
+        <div class="header-content">
+            <div class="content-box active">
+                <a class="" active href="/">Home</a>
+            </div>
+            <div class="content-box">
+                <select name="" id="" class="header-select">
+                    <option value="">Find Room</option>
+                </select>
+            </div>
+            <div class="content-box">
+                <a class="" href="/aboutus">About Us</a>
+            </div>
+            <div class="content-box">
+                <a class="" href="/contactus">Contact Us</a>
+            </div>
         </div>
-
-    </nav>
-</body>
-
-</html>
+        @auth
+        <div class="header-button">
+            <a class="sign-button" href="/profile">
+                <p class="">Profile</p>
+                <img src="/images/contact.png" alt="">
+            </a>
+        </div>
+        @endauth
+        @guest
+        <div class="header-button">
+            <a class="sign-button" href="/login">
+                <p class="">Login</p>
+                <img src="/images/contact.png" alt="">
+            </a>
+        </div>
+        @endguest
+    </div>
+</div>
