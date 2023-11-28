d1d556969f9a0a7c5d22e88d815f92d8438f3316
StefanieVW <esveweee@gmail.com>
2023-11-28 20:24:04 +0700

    TIA

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -11,64 +11,58 @@
 <script src="https://kit.fontawesome.com/aa7454d09f.js" crossorigin="anonymous"></script>
 
 <body>
-    <div class="footer">
-        <section class="contact-form">
-            <h4 class="sectionHeader">contact us</h4>
-            <h1 class="heading">Get In Touch!</h1>
-            <p class="para">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor eos
-                inventore omnis aliquid rerum alias molestias.</p>
+    <footer>
+        <div class="container_footer">
+            <div class="row">
+                <div class="col" id="company">
+                    <img src="images/logo.png" alt="" class="logo">
+                    <p>
+                    Now Everyone Can Learn Music.
+                    </p>
+                    <div class="social">
+                        <a href="#"><i class="fab fa-facebook"></i></a>
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                        <a href="#"><i class="fab fa-youtube"></i></a>
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                        <a href="#"><i class="fab fa-linkedin"></i></a>
+                    </div>
+                </div>
 
-            <div class="contactForm">
-                <form action="#">
-                    <h1 class="sub-heading">Need Support !</h1>
-                    <p class="para para2">Contact us for a quote , help to join the them.</p>
-                    <input type="text" class="input" placeholder="your name">
-                    <input type="text" class="input" placeholder="your email">
-                    <input type="text" class="input" placeholder="your Subject">
-                    <textarea class="input" cols="30" rows="5" placeholder="Your message..."></textarea>
-                    <input type="submit" class="input submit" value="Send Message">
-                </form>
 
-                <div class="map-container">
-                    <div class="mapBg"></div>
-                    <div class="map">
-                        <iframe
-                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.298370414998!2d106.87548617461825!3d-6.224333360959129!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f324a4f3d8ad%3A0x610648e929845480!2sMall%40Bassura!5e0!3m2!1sen!2sid!4v1699969309456!5m2!1sen!2sid"
-                            width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
-                            referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <div class="col" id="services">
+                    <h3>Services</h3>
+                    <div class="links">
+                        <a href="#">Illustration</a>
+                        <a href="#">Creatives</a>
+                        <a href="#">Poster Design</a>
+                        <a href="#">Card Design</a>
+                    </div>
+                </div>
+
+                <div class="col" id="useful-links">
+                    <h3>Links</h3>
+                    <div class="links">
+                        <a href="#">About</a>
+                        <a href="#">Services</a>
+                        <a href="#">Our Policy</a>
+                        <a href="#">Help</a>
+                    </div>
+                </div>
+
+                <div class="col" id="contact">
+                    <h3>Contact</h3>
+                    <div class="contact-details">
+                        <i class="fa fa-location"></i>
+                        <p>FF-42, Procube Avenue, <br> NY, USA</p>
+                    </div>
+                    <div class="contact-details">
+                        <i class="fa fa-phone"></i>
+                        <p>+1-8755856858</p>
                     </div>
                 </div>
             </div>
-
-            <div class="contactMethod">
-                <div class="method">
-                    <i class="fa-solid fa-location-dot contactIcon"></i>
-                    <article class="text">
-                        <h1 class="sub-heading">Location</h1>
-                        <p class="para">Mall Bassura, Aeon Mall Jakarta Garden City, MaxxBox Lippo Mall</p>
-
-                    </article>
-                </div>
-
-                <div class="method">
-                    <i class="fa-solid fa-envelope contactIcon"></i>
-                    <article class="text">
-                        <h1 class="sub-heading">Instagram</h1>
-                        <p class="para">rhapsodiemusicspace</p>
-                    </article>
-                </div>
-
-                <div class="method">
-                    <i class="fa-solid fa-phone contactIcon"></i>
-                    <article class="text">
-                        <h1 class="sub-heading">Phone</h1>
-                        <p class="para">0811-1379-279</p>
-                    </article>
-                </div>
-            </div>
-    </div>
-    </section>
-
+        </div>
+    </footer>
 </body>
 
 </html>
