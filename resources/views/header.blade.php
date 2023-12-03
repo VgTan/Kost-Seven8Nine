019d3ac8ed37e1704db64557caea0cf5cbfa8c21
VgTan <alfonsusvega9@gmail.com>
2023-12-03 21:28:11 +0700

    Revert "Revert "Merge branch 'stepi' into Vega""
    This reverts commit aea6c40a61712c2ee785caf17abb772c5872e918.

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -6,80 +6,74 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/css/header.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@3.2.0/fonts/remixicon.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"> -->
 </head>
 
 <body>
-    <div class="header-container">
-        <div class="header">
-            <div class="name">
+    <header>
+        <div class="container">
+            <input type="checkbox" name="" id="check">
+
+            <div class="logo-container">
                 <a href="/" class="logo"><img src="/images/logo.png" alt=""></a>
             </div>
-            <div class="header-content">
-                <div class="content-box active">
-                    <a class="" active href="/">Home</a>
+
+            <div class="nav-btn">
+                <div class="nav-links">
+                    <ul>
+                        <li class="nav-link" style="--i: .6s">
+                            <a href="/">Home</a>
+                        </li>
+                        <li class="nav-link" style="--i: .6s">
+                            <a href="/aboutus">About Us</a>
+                        </li>
+                        <li class="nav-link" style="--i: 1.35s">
+                            <a href="/contactus">Contact Us</a>
+                        </li>
+                    </ul>
                 </div>
-                <div class="content-box">
-                    <a class="" href="/aboutus">About Us</a>
-                </div>
-                <div class="content-box">
-                    <a class="" href="/contactus">Contact Us</a>
-                </div>
-            </div>
-            <div class="head-end">
-                <form class="search-form" action="{{ route('findroom', ['room' => ':room']) }}" method="GET">
-                    <div class="search-container">
-                        <i class="fa fa-search" aria-hidden="true"></i>
-                        <input class="search" type="text" name="room" placeholder="Search Room/Branch">
-                    </div>
-                </form>
                 @auth
-                <div class="header-button">
-                    <a class="sign-button" href="/profile">
+                <div class="log-sign" style="--i: 1.8s">
+                    <form class="search-container" action="{{ route('findroom', ['room' => ':room']) }}" method="GET">
+                        <div class="search-wrapper">
+                            <i class="ri-search-line"></i>
+                            <input class="searchInput" type="text" name="room" placeholder="Search Room/Branch">
+                        </div>
+                    </form>
+
+                    <a href="/profile" class="btn solid">
                         <p class="">Profile</p>
                         <img src="/images/contact.png" alt="">
                     </a>
                 </div>
                 @endauth
                 @guest
-                <div class="header-button">
-                    <a class="sign-button" href="/login">
+                <div class="log-sign" style="--i: 1.8s">
+                    <form class="search-container" action="{{ route('findroom', ['room' => ':room']) }}" method="GET">
+                        <div class="search-wrapper">
+                            <i class="ri-search-line"></i>
+                            <input class="searchInput" type="text" name="room" placeholder="Search Room/Branch">
+                        </div>
+                    </form>
+
+                    <a href="/login" class="btn solid">
                         <p class="">Login</p>
                         <img src="/images/contact.png" alt="">
                     </a>
                 </div>
                 @endguest
             </div>
-        </div>
-    </div>
 
-    <!-- <nav class="navbar navbar-expand-lg bg-light">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="/"><img src="/images/logo.png" alt=""></a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/aboutus">About Us</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/contactus">Contact Us</a>
-                </li>
-            </ul>
-            <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
+            <div class="hamburger-menu-container">
+                <div class="hamburger-menu">
+                    <div></div>
+                </div>
+            </div>
         </div>
-    </div>
-</nav> -->
-
-    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script> -->
+    </header>
 </body>
 
 </html>
