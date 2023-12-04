53bd847e071f9689d6acc0486e192bd1099f4a70
VgTan <alfonsusvega9@gmail.com>
2023-12-04 20:29:17 +0700

    feat:

--- resources/views/mail.blade.php
+++ resources/views/mail.blade.php
@@ -1,1 +1,16 @@
-hi {{ $token->bundle }}
+Subject: Booking Confirmation - Payment Proof Attached
+
+A new order of tokens has been made on the website. Here are the details:
+
+Booking Information:
+- User: {{ $token->name }}
+- Order ID: {{ $token->id }}
+- Date: {{ $token->created_at }}
+- Bundle: {{ $token->bundle }}
+- Price: {{ $token->price }}
+
+Payment Proof:
+
+
+Please confirm the payment on the website.
+Thank you!
